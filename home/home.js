5516497ee09b23b311485d5c191e2fbafdb830db
duoduoli <leer561@gmail.com>
2019-03-25 17:25:55 +0800

    完成导航

---
+++ home/home.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+export default class Home extends Component{
+	render() {
+		return (
+			<View >
+				<Text>Welcome to home page</Text>
+			</View>
+		);
+	}
+}
