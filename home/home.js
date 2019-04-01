e31594618068973b81ac89a203f723c0f553448d
lipeng <leer.li@ian-soft.com>
2019-04-01 14:55:37 +0800

    完成V2ex数据展示

--- home/home.js
+++ home/home.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
 import Counter from './components/counter'
+import News from './components/news'
 
 export default class Home extends Component{
 	render() {
 		return (
+			<View style={{flex:1}}>
 				<Counter />
+				<News />
+			</View>
+
 		);
 	}
 }
